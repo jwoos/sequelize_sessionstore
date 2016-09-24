2116f89947682c8a72cf946c44866c4e8a036b06
Jun Woo Shin <shin.junwoo97@gmail.com>
2016-09-24 15:53:54 -0400

    integration test begin

--- test/integration/index.js
+++ test/integration/index.js
@@ -1,3 +1,11 @@
 'use strict';
 
-describe('Integration Test |', () => {});
+const path = require('path');
+
+const bluebird = require('bluebird');
+const session = require('express-session');
+
+const SequelizeSessionStore = require(path.join(__dirname, '../../dist/index'))(session.Store);
+const errors = require(path.join(__dirname, '../../dist/errors'));
+
+describe('Sequelize Session Store', () => {});
